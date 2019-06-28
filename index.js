1e6d98f42cfc21e01ca89b97bb8f938ca46a2004
Reid Miller <rmiller9@Reids-MacBook-Air.local>
2019-06-28 12:30:38 -0700

    Done

--- index.js
+++ index.js
@@ -8,7 +8,20 @@ const passport = require('./config/passportConfig');
 // module for flash messages
 const flash = require('connect-flash');
 const isLoggedIn = require('./middleware/isLoggedIn');
+const helmet = require('helmet');
+
+// This is only used by the session store
+const db = require('./models');
+
 const app = express();
+
+// This line makes the session use sequelize to write session data to a postgres table
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sessionStore = new SequelizeStore({
+  db: db.sequelize,
+  experation: 1000 * 60 * 30
+});
 
 app.set('view engine', 'ejs');
 
@@ -16,13 +29,18 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts);
+app.use(helmet());
 
 // Configures express-session middleware
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: sessionStore
 }));
+
+// Use this line once to set up the store table
+sessionStore.sync();
 
 // Starts the flash middleware
 app.use(flash());
